a0481a89e579349aa8f9cd7e396634e08fc80633
Ageychev Denis <ageychevdenis@gmail.com>
2022-03-26 18:05:53 +0900

    homework_3

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -17,12 +17,15 @@ class EmployersAddForm extends Component {
    }
 
    onSubmit = (e) => {
-      e.preventDefault()
-      this.props.onAdd(this.state.name, this.state.salary);
-      this.setState({
-         name: '',
-         salary: ''
-      })
+      e.preventDefault();
+      if (this.state.name !== '' && this.state.salary !== '') {
+         this.props.onAdd(this.state.name, this.state.salary);
+         this.setState({
+            name: '',
+            salary: ''
+         })
+      }
+
    }
 
    render() {
