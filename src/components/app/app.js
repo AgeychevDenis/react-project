84176b2b3c67bd0d64214c2767e333f09f87df8a
Ageychev Denis <ageychevdenis@gmail.com>
2022-03-26 13:35:03 +0900

    homework_1

---
+++ src/components/app/app.js
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-fliter/app-filter';
+import EmployersList from '../emoloyers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+
+import './app.css';
+
+class App extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: [
+            { name: 'Jhon E.', salary: 800, increase: false, id: 1 },
+            { name: 'Den A.', salary: 3000, increase: true, id: 2 },
+            { name: 'Alex M.', salary: 5000, increase: false, id: 3 }
+         ]
+      }
+   }
+
+   deleteItem = (id) => {
+      this.setState(({ data }) => {
+         const index = data.findIndex(elem => elem.id === id);
+
+      })
+   }
+
+   render() {
+      return (
+         <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+               <SearchPanel />
+               <AppFilter />
+            </div>
+
+            <EmployersList
+               data={this.state.data}
+               onDelete={this.deleteItem} />
+            <EmployersAddForm />
+
+         </div>
+      );
+   }
+}
+
+export default App;
